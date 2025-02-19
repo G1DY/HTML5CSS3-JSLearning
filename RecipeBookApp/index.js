500520ce0572ac676a7cb1fcd7d3192cfb00b04b
gideon <kipkerring.gedion@gmail.com>
2025-02-19 15:00:09 +0300

    fetching data from recipe API and displaying it on the DOM

--- RecipeBookApp/index.js
+++ RecipeBookApp/index.js
@@ -1,4 +1,33 @@
-const recipeListEl = document.getElementById("recipe-list");
+const recipeListEl = document.getElementById("recipe-list"); //parent element
+
+// appending children to the parent and dispaly rendering to DOM
+const dispayRecipes = (recipes) => {
+  recipeListEl.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe-list");
+
+    const recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = "recipe-item";
+
+    const recipeTitleEl = document.createElement("h2");
+    recipeTitleEl.innerHTML = recipe.title;
+
+    const recipeIngredientsEl = document.createElement("p");
+    recipeIngredientsEl.innerHTML = `<strong>Ingredients: </strong>${recipe.extendIngredients.map(
+      (ingredient) => ingredient.original.join(", ")
+    )}`;
+
+    const recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.srcUrl;
+    recipeLinkEl.innerHTML = "View Recipe";
+
+    recipeListEl.appendChild(recipeImageEl);
+    recipeListEl.appendChild(recipeTitleEl);
+    recipeListEl.appendChild(recipeItemEl);
+  });
+};
 
 const getRecipes = async () => {
   const res = await fetch();
@@ -8,5 +37,6 @@ const getRecipes = async () => {
 
 const init = async () => {
   const recipes = await getRecipes();
+  dispayRecipes(recipes);
 };
 init();
